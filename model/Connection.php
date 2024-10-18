798b5670eb4779b71236f861ba1b0e2430b32a21
SEBASTIAN SICCUAN <2233205@slu.edu.ph>
2024-10-18 05:51:23 +0000

    Update Connection.php

--- model/Connection.php
+++ model/Connection.php
@@ -1,16 +1,11 @@
 <?php
-
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "boothlink"; 
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
-echo "i love you";
