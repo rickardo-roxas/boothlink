a0a23943d3283c85c76d632c28c0677e98232bae
Sebastian Siccuan <2233205@slu.edu.ph>
2024-10-19 16:17:40 +0800

    Fixed Login

--- model/Connection.php
+++ model/Connection.php
@@ -1,10 +1,16 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "boothlink"; 
+$host = "localhost"; 
+$username = "root"; 
+$password = ""; 
+//uncomment line 6 if using windows
+$conn = new mysqli($host, $username, $password, $dbname);
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+//uncomment line 9-10 if using mac
+// $dbname = "boothlink"; 
+// $socket = "/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock";  
+
+//uncomment line 13 if using mac
+// $conn = new mysqli($host, $username, $password, $dbname, null, $socket); 
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
