e7a2a5508dcfca07cb58e9181e0beab638efed73
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2024-11-03 00:48:53 +0800

    Initial version of PageHandler and Router

--- controller/vendor/core/PageHandler.php
+++ controller/vendor/core/PageHandler.php
@@ -1,74 +1,22 @@
 <?php
 
-class PageHandler {
-
-    public function __construct() {
-        /** -------->  TODO Hardcoded values while organization selector has not been.  */
-        $_SESSION['orgPhoto'] = '../../assets/images/placeholder.jpeg';
-        $_SESSION['orgName'] = 'SCHEMA';
-        $_SESSION['orgID'] = 1;
-        $_SESSION['handler'] = serialize($this);
-    }
-
-    public function loadPage($page) {
-        switch ($page) {
-            case 'home':
-                $this->loadDashboard(true);
-                break;
-            case 'reservations':
-                $this->loadReservations();
-                break;
-            case 'products':
-                $this->loadProducts();
-                break;
-            case 'schedule':
-                $this->loadSchedule();
-                break;
-            case 'sales':
-                $this->loadSales();
-                break;
-            default:
-                $this->loadDashboard(false);
-                break;
+/**
+ * Handles the rendition of pages.
+ */
+class PageHandler
+{
+    /**
+     * Renders the view of a given webpage passed from the Router.
+     * @param $path - The path passed from the Router.
+     * @return void
+     */
+    public function render($path) {
+        $view = __DIR__ . '/../../../view/vendor/' . $path . '/'.  $path . '_view' . '.php';
+        if (file_exists($view)) {
+            include $view;
+        } else {
+            http_response_code(404);
+            echo "404 Not Found.";
         }
-    }
-
-    public function loadDashboard($firstTime) {
-        require_once __DIR__ . '/../home/DashboardController.php';
-        $controller = new DashboardController();
-        $controller->index($firstTime);
-    }
-
-    public function loadReservations() {
-        require_once __DIR__ . '/../reservations/ReservationsController.php';
-        $controller = new ReservationsPageController();
-        $controller->index();
-    }
-
-    public function loadProducts() {
-        require_once __DIR__ . '/../../vendor/products/ProductsController.php';
-        $controller = new ProductsPageController();
-        $controller->index();
-    }
-
-    public function loadSchedule() {
-        require_once __DIR__ . '/../../vendor/schedule/ScheduleController.php';
-        $controller = new ScheduleController();
-        $controller->index();
-    }
-
-    public function loadSales() {
-        require_once __DIR__ . '/../../vendor/sales/SalesController.php';
-        $controller = new SalesController();
-        $controller->index();
-    }
-
-    public function getUsername() {
-        return $this -> {$_SESSION['user']};
-    }
-
-
-    public function getFromURL() {
-        $this->loadPage($_GET['page']);
     }
 }
--- controller/vendor/core/Router.php
+++ controller/vendor/core/Router.php
@@ -1,40 +1,37 @@
 <?php
 
-class Router {
-    public function route($requestUri) {
-        $uri = parse_url($requestUri, PHP_URL_PATH);
-        $page = trim($uri, '/');
+/**
+ * Handles requests and maps to respective controllers.
+ */
+class Router
+{
+    /**
+     * @var array Holds HTTP methods and maps the paths to the handler function.
+     */
+    private $routes = [];
 
-        switch ($page) {
-            case 'cs-312_boothlink/home':
-                $this->loadPage('home');
-                break;
-
-            case 'cs-312_boothlink/reservations':
-                $this->loadPage('reservations');
-                break;
-
-            case 'cs-312_boothlink/products':
-                $this->loadPage('products');
-                break;
-
-            case 'cs-312_boothlink/schedule':
-                $this->loadPage('schedule');
-                break;
-
-            case 'cs-312_boothlink/sales':
-                $this->loadPage('sales');
-                break;
-
-            default:
-                // Handle 404 Not Found
-                $this->loadPage('404');
-                break;
-        }
+    /**
+     * Adds a new route with a specified method, path, and handler.
+     * @param $method - specified HTTP method (GET, POST, etc.)
+     * @param $path - the path of the webpage (/home, /about, etc.)
+     * @param $handler - the corresponding function for the method and path
+     * @return void
+     */
+    public function addRoute($method, $path, $handler) {
+        $this->routes[$method][$path] = $handler;
     }
 
-    private function loadPage($page) {
-        $pageHandler = new PageHandler();
-        $pageHandler->loadPage($page);
+    public function handleRequest() {
+        $method = $_SERVER['REQUEST_METHOD'];
+        $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+        // Responds with 404 when method and path is not a set variable in the routes array
+        if (isset($this->routes[$method][$path])) {
+            $handler = $this->routes[$method][$path];
+            call_user_func($handler);
+        } else {
+            http_response_code(404);
+            echo "404 Not Found";
+        }
     }
 }
