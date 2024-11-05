66306cb109638ca65d9a139ddc0934d2d886da3c
MARVIN PATRICK LACANILAO <2230153@slu.edu.ph>
2024-11-06 02:39:03 +0800

    Pop up integration of calendar form

--- view/vendor/schedule/schedule_view.php
+++ view/vendor/schedule/schedule_view.php
@@ -1,17 +1,22 @@
-<?php include ('view/page-fragments/header.php'); ?>
-<link rel="stylesheet" href="<?php echo BASE_URL?>/public/css/vendor/schedule.css"></link>
+<?php include('view/page-fragments/header.php'); ?>
+<link rel="stylesheet" href="<?php echo BASE_URL ?>/public/css/vendor/schedule.css">
+<link rel="stylesheet" href="<?php echo BASE_URL ?>/public/css/vendor/schedule_selector.css">
+<script src="<?php echo BASE_URL ?>/public/javascript/vendor/schedule.js" defer></script>
+<script src="<?php echo BASE_URL ?>/public/javascript/vendor/schedule_selector.js" defer></script>
 
 <main>
     <div class="main-table">
         <div class="table-header">
             <p>Schedule</p>
             <div class="action-buttons">
-                <button class="add-button">
+                <button class="add-button" onclick="openSchedulePopup()">
                     <img src="../../../assets/icons/add-blue-outline.png" alt="Add Icon" class="add-icon">
                     Add Schedule
                 </button>
             </div>
         </div>
+        
+        <!-- Schedule Table -->
         <div class="table-schedule">
             <table>
                 <thead>
@@ -26,19 +31,26 @@
                 </thead>
                 <tbody>
                     <?php for ($i = 0; $i < 7; $i++): ?>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
                     <?php endfor; ?>
                 </tbody>
             </table>
         </div>
     </div>
+    
+    <!-- Popup -->
+    <div id="schedule-popup" onclick="closeSchedulePopup()">
+        <div class="popup-content" onclick="event.stopPropagation();">
+            <?php include 'schedule_selector_view.php'; ?>
+        </div>
+    </div>
 </main>
 
-<?php require 'view/page-fragments/Footer.php'?>
+<?php require 'view/page-fragments/Footer.php' ?>
