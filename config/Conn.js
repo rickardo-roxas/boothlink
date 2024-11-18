f1b799fd4c946ab77340c01ea8d5378769399a00
Rithik Tank <2233293@slu.edu.ph>
2024-11-18 10:01:18 +0800

    Created the queries for shop page

--- config/Conn.js
+++ config/Conn.js
@@ -10,25 +10,7 @@ const conn = sql.createPool({
     waitForConnections: true,
 });
 
-conn.connect((err) => {
-
-    if (err) {
-        console.log("Connection error: ", err.stack);
-        return;
-    }
-});
-
-
-function closeConnection() {
-    conn.end((err) => {
-        if (err) {
-            console.log(err);
-            return
-        }
-    })
-}
 
 module.exports = {
-    conn, 
-    closeConnection
+    conn
 }
