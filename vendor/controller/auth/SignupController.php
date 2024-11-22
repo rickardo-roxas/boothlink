dacdb4cc79dc4fac7031b7399f5bc17dd4760cfb
Sebastian Siccuan <2233205@slu.edu.ph>
2024-11-22 18:57:56 +0800

    Sign Up working, but need help with routing

--- vendor/controller/auth/SignupController.php
+++ vendor/controller/auth/SignupController.php
@@ -2,23 +2,41 @@
 
 namespace controller\auth;
 
+use model\vendor\SignUp;
 
 require_once "vendor/config/Connection.php";
-// require_once "vendor/model/auth/Signup.php";
-require_once 'vendor/view/auth/signup_view.php';
+require_once "vendor/model/auth/SignUp.php";
 
-class SignupController
-{
+class SignupController {
     protected $conn;
+    private $model;
 
     public function __construct($conn) {
         $this->conn = $conn;
+        $this->model = new SignUp($conn);
     }
 
     public function handleSignup() {
-        // code here
-        require_once 'vendor/view/auth/signup_view.php';
-
+        if ($_SERVER['REQUEST_URI'] === '/cs-312_boothlink/signup' && $_SERVER['REQUEST_METHOD'] === 'GET') {
+            require_once 'vendor/view/auth/signup_view.php';
+        }
+        include 'vendor/view/auth/signup_view.php';
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+            $last_name = $_POST['last_name'];
+            $first_name = $_POST['first_name'];
+            $email = $_POST['email'];
+            try {
+                $success = $this->model->createAccount($email, $username,$last_name, $first_name, $password);
+                if ($success) {
+                    header('Location: /cs-312_boothlink/login');
+                } else {
+                    echo "Error creating account.";
+                }
+            } catch (\Exception $e) {
+                echo "An error occurred: " . $e->getMessage();
+            }
+        }
     }
-
 }
