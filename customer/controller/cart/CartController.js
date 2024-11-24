f81cfa1c220f6788dce16cc2ef4bee81cb69008b
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2024-11-24 14:25:14 +0800

    Finished cart and checkout frontend

--- customer/controller/cart/CartController.js
+++ customer/controller/cart/CartController.js
@@ -2,8 +2,12 @@ const model = require('../../model/cart/Cart');
 
 const index = (req, res) => {
 
-    res.render("cart/cart_view", { title : "Home", logo : "temp", sample : model.sample})
-
+    res.render("cart/cart_view", 
+        {
+            title : "Cart", 
+            logo : "temp", 
+            sample : model.sample
+        })
 }
 
 module.exports = {
