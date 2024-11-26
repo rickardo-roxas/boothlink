3f77dbd9237f3301e5d5c37bc69da5364253ba1a
Rithik Tank <2233293@slu.edu.ph>
2024-11-27 00:18:35 +0800

    Began add to cart

--- customer/controller/shop/ShopProductController.js
+++ customer/controller/shop/ShopProductController.js
@@ -4,16 +4,18 @@ const model = require("../../model/shop/ShopProduct");
 
 function index(id, req, res) {
     let productPromise = model.getProductByID(id);
+    let schedulesPromise = model.getSchedulesByProductID(id);
 
-    Promise.all([productPromise]).then (values => {
+    Promise.all([productPromise, schedulesPromise]).then(results => {
         res.render("shop/shop_product_view", {
             title: "Shop Product",
     
             // Dynamic Objects
-            product : values[0],
+            product : results[0],
+            schedules : results[1]
         });
-
     });
+    
 }
 
 
