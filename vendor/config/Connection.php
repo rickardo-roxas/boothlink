41c04d926e8c1988ce446ad1bf005f7db8289eda
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2024-11-26 11:12:10 +0800

    Fixed connection.php

--- vendor/config/Connection.php
+++ vendor/config/Connection.php
@@ -4,14 +4,14 @@ $username = "root";
 $password = "root";
 $dbname = "boothlink";
 //uncomment line 6 if using windows
-// $conn = new mysqli("database", $username, $password, $dbname);
+$conn = new mysqli("database", $username, $password, $dbname);
 
 // uncomment line 9-10 if using mac
-$dbname = "boothlink";
-$socket = "/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock";
+//$dbname = "boothlink";
+// $socket = "/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock";
 
 //uncomment line 13 if using mac
-$conn = new mysqli($host, $username, $password, $dbname, null, $socket);
+// $conn = new mysqli($host, $username, $password, $dbname, null, $socket);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
